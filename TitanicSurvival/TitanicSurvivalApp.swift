10e5e1a937331c960c3e1ea58647ddd919e88eb5
charlieroth <charlieroth4@gmail.com>
2024-05-25 19:29:51 +0200

    Initial Commit

---
+++ TitanicSurvival/TitanicSurvivalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TitanicSurvivalApp.swift
+//  TitanicSurvival
+//
+//  Created by Charles Roth on 2024-05-25.
+//
+
+import SwiftUI
+
+@main
+struct TitanicSurvivalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
